9db94aebfbc2924492cf8ebac25e772bed4f3563
Santamaria Joaquin <joasantama2020@outlook.com>
2026-03-10 12:53:13 -0300

    Fix: rutas corregidas, JSON formateado y managers funcionando; Reentrega de Preentrega1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-import productsRouter from "./routes/products.router.js";
-import cartsRouter from "./routes/carts.router.js";
+import express from "express";
+import productsRouter from "../routes/products.router.js";
+import cartsRouter from "../routes/cart.router.js";
 
 const app = express();
 app.use(express.json());
@@ -8,6 +8,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+
+app.get("/", (req, res) => {
+  res.send("Bienvenido a la API de Preentrega 1");
+});
 
 app.listen(8080, () => {
   console.log("Servidor escuchando en puerto 8080");
